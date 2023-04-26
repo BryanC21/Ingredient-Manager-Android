b80ad1e01158d2d66c57f5ecdc1b0d589dbaeb08
Bryan Caldera <bryanicaldera@gmail.com>
2023-04-25 22:50:51 -0700

    save state of recipe list and block spamming of request button

--- app/src/main/java/com/sjsu/hackathon/ingredient_manager/RecipeList.java
+++ app/src/main/java/com/sjsu/hackathon/ingredient_manager/RecipeList.java
@@ -34,6 +34,13 @@ import java.util.Date;
 public class RecipeList extends Fragment implements RecipeListener {
 
     private View rootView;
+
+    private boolean isRecipeControllerExecuted = false;
+
+    private boolean loadingLock = false;
+
+    private RecyclerView recyclerView;
+    private RecipeListAdapter adapter;
     private NavController navController;
 
     private ArrayList<Recipe> recipeList;
@@ -49,8 +56,6 @@ public class RecipeList extends Fragment implements RecipeListener {
     }
     public static RecipeList newInstance() {
         RecipeList fragment = new RecipeList();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -61,47 +66,84 @@ public class RecipeList extends Fragment implements RecipeListener {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView =  inflater.inflate(R.layout.fragment_recipe_list, container, false);
         navController = NavHostFragment.findNavController(this);
-
         regen = rootView.findViewById(R.id.regen_button);
         regen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("regen", "beginning regen");
-                rootView.findViewById(R.id.notifications_loading2).setVisibility(View.VISIBLE);
-                recipeController.getRecipe(ingredientList, 2);
+                //Simultaneous requests cause issues
+                //Doesn't crash but causes inconsistent results
+                if(!loadingLock) {
+                    Log.d("regen", "beginning regen");
+                    rootView.findViewById(R.id.notifications_loading2).setVisibility(View.VISIBLE);
+                    try {
+                        loadingLock = true;
+                        recipeController.getRecipe(ingredientList, 2);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        loadingLock = false;
+                        Toast.makeText(rootView.getContext(), "Generate Recipes Failed. Try again.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Log.d("GPT loading", "already in progress");
+                    Toast.makeText(rootView.getContext(), "Please wait for recipes to load.", Toast.LENGTH_LONG).show();
+                }
             }
         });
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-        rootView =  inflater.inflate(R.layout.fragment_recipe_list, container, false);
-
-        if (getArguments() != null) {
-
-            //recipeList = getArguments().getParcelableArrayList("recipes");
+        if (getArguments() != null && !isRecipeControllerExecuted) {
+            // Generate recipe list
             ArrayList<String> ingredientsTemp = getArguments().getStringArrayList("ingredients");
             ingredientList = new ArrayList<>();
             recipeList = new ArrayList<>();
+            recipeController = new RecipeController(getContext(), this);
 
             for(int i = 0; i < ingredientsTemp.size(); i++){
                 //TODO very bad, fix this, string to ingredient
                 ingredientList.add(new Ingredient(ingredientsTemp.get(i), 1.0, "none", "none", new Date(), new Date(), "none", "none", "none"));
             }
-
-            recipeController = new RecipeController(getContext(), this);
             Log.d("Attempt GPT", "starting");
             rootView.findViewById(R.id.notifications_loading2).setVisibility(View.VISIBLE);
-            recipeController.getRecipe(ingredientList, 2);
+            try {
+                loadingLock = true;
+                recipeController.getRecipe(ingredientList, 2);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(rootView.getContext(), "Generate Recipes Failed. Try again.", Toast.LENGTH_LONG).show();
+                //If the first generation fails, go back to the previous fragment
+                navController.popBackStack();
+            }
+        } else if(isRecipeControllerExecuted && recipeList != null){
+            // Set existing recipe list
+            Log.d("RecipeList", "--------count: " + this.recipeList.size());
+            loadingLock = false; // just in case
+            recyclerView = rootView.findViewById(R.id.recipe_list_recycler_view);
+            adapter = new RecipeListAdapter(this.recipeList);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setAdapter(adapter);
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //outState.putBoolean("isRecipeControllerExecuted", isRecipeControllerExecuted);
+        //outState.putParcelableArrayList("recipeList", recipeList);
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        /*if (savedInstanceState != null) {
+            isRecipeControllerExecuted = savedInstanceState.getBoolean("isRecipeControllerExecuted");
+            recipeList = savedInstanceState.getParcelableArrayList("recipeList");
+        }*/
     }
 
     @Override
@@ -118,7 +160,8 @@ public class RecipeList extends Fragment implements RecipeListener {
     @Override
     public void onDataFail(String reason) {
         rootView.findViewById(R.id.notifications_loading2).setVisibility(View.INVISIBLE);
-        Toast.makeText(getContext(), "Generate Recipes Failed. Try again.", Toast.LENGTH_LONG).show();
+        loadingLock = false;
+        Toast.makeText(rootView.getContext(), "Generate Recipes Failed. Try again.", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -127,13 +170,15 @@ public class RecipeList extends Fragment implements RecipeListener {
         Log.d("RecipeList", "count: " + recipeList.size());
         Log.d("RecipeListGlobal", "count: " + this.recipeList.size());
         rootView.findViewById(R.id.notifications_loading2).setVisibility(View.INVISIBLE);
+        loadingLock = false;
         for(Recipe r : recipeList){
             this.recipeList.add(r);
         }
         //recipeController.clearHistory();
-        RecyclerView recyclerView = rootView.findViewById(R.id.recipe_list_recycler_view);
-        RecipeListAdapter adapter = new RecipeListAdapter(this.recipeList);
+        recyclerView = rootView.findViewById(R.id.recipe_list_recycler_view);
+        adapter = new RecipeListAdapter(this.recipeList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+        isRecipeControllerExecuted = true;
     }
 }
